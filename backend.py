326e2dae2ba71ae9285fab32aab8fb8e88a4c11a
Ruxandra <filotiruxandra@gmail.com>
2025-05-31 11:08:48 +0300

    app complete

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "cc2c3950e72332fb590d21b78b944327"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -13,15 +13,8 @@ def get_data(place, forecast_days, kind):
     #calculate the values of the weather for 24 days (filter the data by forecast days)
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-
-    #filter by kind (Temperature or Sky) parameter
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data] # main is the dictionary and temp is a key in dictionary list
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data] #weather is the dictionary and main is key in dictionary list
-        #[0] extracts the dictionary because is one single dictionary in this weather list -> weather:[{'': '', '': '', '': ''}]
     return filtered_data 
     
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
+
+#add title, text input, selectbox and subheader
 
 st.title("Weather forecast for the next days")
 
@@ -9,11 +12,35 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-#plot data dynamically -> get_data function takes the data from api
-d, t = get_data(place, days, option)
+if place:
+    try:
+        #get the temperature or sky data 
+        filtered_data = get_data(place, days)
 
+        if option == "Temperature":
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})   # Create a simple line chart using Plotly Express
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data] # main is the dictionary and temp is a key in dictionary list
+           
+            dates = [dict["dt_txt"] for dict in filtered_data]
+           
+            #create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})   # Create a simple line chart using Plotly Express
+            #adding a graphic
+            st.plotly_chart(figure)
 
-#adding a graphic
-st.plotly_chart(figure)
+        else:
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            #"Clear" is a key from dictionary of weather and it refers to sky
+           
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data] #weather is the dictionary and main is key in dictionary list
+            #[0] extracts the dictionary because is one single dictionary in this weather list -> weather:[{'': '', '': '', '': ''}]
+            
+            image_paths = [images[condition] for condition in sky_conditions]
+
+            #displays date and time below every image
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            for img, date in zip(image_paths, dates):
+                st.image(img, width=115, caption=date)
+                
+    except KeyError: 
+        st.write("That place does not exist")
