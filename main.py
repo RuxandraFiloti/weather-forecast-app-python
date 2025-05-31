283797cb6bf551583006ad5debdfca574fb4c1e1
Ruxandra <filotiruxandra@gmail.com>
2025-05-31 10:16:21 +0300

    api key for weather data

--- main.py
+++ main.py
@@ -10,17 +10,8 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 #plot data dynamically -> get_data function takes the data from api
-def get_data(days):
-    
-    dates = ["2023-10-01", "2023-10-02", "2023-10-03", "2023-10-04", "2023-10-05"]
+d, t = get_data(place, days, option)
 
-    temperatures = [20, 22, 21, 19, 18]  # Example temperature data MUST HAVE the same length as dates
-
-    temperatures = [days * i for i in temperatures]  # Scale temperatures by the number of days selected
-
-    return dates, temperatures
-
-d, t = get_data(days)  # Call the function to get data
 
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})   # Create a simple line chart using Plotly Express
 
